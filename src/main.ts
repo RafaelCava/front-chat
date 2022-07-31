d377f9ea36c4ca205a2796471f1fc6d365c8ef4f
rafael cavalcante <ra.facavalcante@hotmail.com>
2022-07-31 13:51:36 -0300

    feat(axios): insert axios on provider global

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,15 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import PrimeVue from "primevue/config";
-import "primevue/resources/themes/saga-blue/theme.css"; //theme
-import "primevue/resources/primevue.min.css"; //core css
+import http from "./services/http";
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
 
-createApp(App).use(store).use(router).use(PrimeVue).mount("#app");
+const app = createApp(App);
+
+app.use(store);
+app.use(router);
+app.use(PrimeVue);
+app.provide("http", http);
+app.mount("#app");
