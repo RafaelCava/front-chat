b98386714cfac816d814f8888be8c652c1ca6fd0
rafael cavalcante <ra.facavalcante@hotmail.com>
2022-07-31 12:24:01 -0300

    feat(feature/view-login): implements a criation to view login

---
+++ src/components/FormSignIn.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="container">
+    <form autocomplete="off" class="form">
+      <h1>Login</h1>
+      <span class="p-float-label">
+        <InputText id="email" type="email" v-model="email" />
+        <label for="email">E-mail</label>
+      </span>
+      <span class="p-float-label">
+        <Password
+          id="password"
+          v-model="password"
+          autocomplete="off"
+          :feedback="false"
+          :toggleMask="true"
+        ></Password>
+        <label for="password">Insira sua senha</label>
+      </span>
+      <div class="container-button">
+        <Button @click="handleSignIn" label="login" class="p-button-primary" />
+        <span @click="$emit('signUp')" class="link-signup"
+          >Deseja se cadastrar ?</span
+        >
+      </div>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { TypeActions } from "@/store/enum/type-actions";
+import { defineAsyncComponent, defineComponent, ref } from "@vue/runtime-core";
+import { useStore } from "../store/index";
+
+export default defineComponent({
+  name: "FormSignUp",
+  components: {
+    InputText: defineAsyncComponent(() => import("primevue/inputtext")),
+    Password: defineAsyncComponent(() => import("primevue/password")),
+    Button: defineAsyncComponent(() => import("primevue/button")),
+  },
+  setup() {
+    const store = useStore();
+    const email = ref("");
+    const password = ref("");
+    return {
+      handleSignIn: () =>
+        store.dispatch(TypeActions.LOGIN, {
+          email: email.value,
+          password: password.value,
+        }),
+      email,
+      password,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.form {
+  width: 100%;
+  max-width: 350px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  gap: 30px;
+  background-color: rgb(82, 82, 214);
+  border-radius: 50px;
+}
+
+.form input {
+  width: 250px;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  background-color: #999;
+}
+
+label {
+  color: black;
+}
+
+.container-button {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+}
+
+.link-signup {
+  cursor: pointer;
+  color: #fff;
+}
+
+.link-signup:active {
+  color: rgb(189, 182, 182);
+}
+</style>
---
+++ src/store/enum/type-actions.ts
@@ -0,0 +1,3 @@
+export enum TypeActions {
+  LOGIN = "LOGIN",
+}
